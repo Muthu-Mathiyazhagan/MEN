3fe9f94c1efc0d322d3e63b26b81ca3b96928e72
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2022-04-04 20:56:02 +0530

    CREATE using both GET and POST method Successfully;

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const courses = [
   { id: 1, name: "MongoDB" },
   { id: 2, name: "Express JS" },
@@ -16,19 +18,38 @@ app.get("/", (req, res) => {
 
 //Send all courses details
 app.get("/api/courses", (req, res) => {
-  res.send(JSON.stringify(courses) + "\n\n" + "Test");
+  res.send(JSON.stringify(courses) + "\n\n" + "Test Text");
 });
 
 //Send only Particular course details
 app.get("/api/courses/:id", (req, res) => {
-  // if (req.query) return res.send(JSON.stringify(req.query));
   const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) {
-    res.status(404).send("The Course of Given ID was not found");
-  }
+  if (!course) res.status(404).send("The Course of Given ID was not found");
   res.send(course);
 
   // res.send(JSON.stringify(courses[req.params.id - 1]) + req.query); //Old Own Logic
+});
+
+//Create course using POST method
+
+app.post("/api/courses", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+//Create course using GET method
+app.get("/api/courses/create/:name", (req, res) => {
+  let names = req.params.name;
+  const course = {
+    id: courses.length + 1,
+    name: names,
+  };
+  courses.push(course);
+  res.send(course);
 });
 
 const port = process.env.PORT || 3000;
