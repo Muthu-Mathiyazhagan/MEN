fac643ad1cc38cdcd6d5889c23b18ecf135959f1
Muthu Mathiyazhagan <muthumathiyazhagan@gmail.com>
2022-04-07 11:44:53 +0530

    Write maintainable code for routes

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,58 +1,16 @@
 const express = require("express");
 const app = express();
 const loggerMiddleware = require("./middleware/logger");
+const courses = require("./routes/courses");
+const home = require("./routes/home");
+
 app.use(loggerMiddleware);
 
 app.use(express.json());
-
-const courses = [
-  { id: 1, name: "MongoDB" },
-  { id: 2, name: "Express JS" },
-  { id: 3, name: "React JS" },
-  { id: 4, name: "Node JS" },
-];
+app.use("/api/courses", courses);
+app.use("/", home);
 
 // console.log(process.env.PORT);
-
-app.get("/", (req, res) => {
-  res.send("Hello Wordl");
-});
-
-//Send all courses details
-app.get("/api/courses", (req, res) => {
-  res.send(JSON.stringify(courses) + "\n\n" + "Test Text");
-});
-
-//Send only Particular course details
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("The Course of Given ID was not found");
-  res.send(course);
-
-  // res.send(JSON.stringify(courses[req.params.id - 1]) + req.query); //Old Own Logic
-});
-
-//Create course using POST method
-
-app.post("/api/courses", (req, res) => {
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-//Create course using GET method
-app.get("/api/courses/create/:name", (req, res) => {
-  let names = req.params.name;
-  const course = {
-    id: courses.length + 1,
-    name: names,
-  };
-  courses.push(course);
-  res.send(course);
-});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening  On http://localhost:${port}`));
---
+++ express-demo/routes/courses.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const router = express.Router();
+
+const courses = [
+  { id: 1, name: "MongoDB" },
+  { id: 2, name: "Express JS" },
+  { id: 3, name: "React JS" },
+  { id: 4, name: "Node JS" },
+];
+//Send all courses details
+router.get("/", (req, res) => {
+  console.log("Courses Router Module");
+
+  res.send(JSON.stringify(courses) + "\n\n" + "Test Text");
+});
+
+//Send only Particular course details
+router.get("/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) res.status(404).send("The Course of Given ID was not found");
+  res.send(course);
+
+  // res.send(JSON.stringify(courses[req.params.id - 1]) + req.query); //Old Own Logic
+});
+
+//Create course using POST method
+
+router.post("/", (req, res) => {
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+//Create course using GET method
+router.get("/create/:name", (req, res) => {
+  let names = req.params.name;
+  const course = {
+    id: courses.length + 1,
+    name: names,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+module.exports = router;
